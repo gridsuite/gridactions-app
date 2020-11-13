e9328df2afb5ebe0ff946c78e1d2e4e4e745a90c
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2020-11-13 12:26:19 +0100

    update setup Proxy
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
+        createProxyMiddleware('http://localhost:5022/api/actions-server', {
+            pathRewrite: { '^/api/actions-server/': '/' },
+        })
+    );
+    app.use(
         createProxyMiddleware('http://localhost:9000/api/gateway', {
             pathRewrite: { '^/api/gateway/': '/' },
         })
