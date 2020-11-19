5ee8e315a2a34af12b6f7c9c6d7a11e33cab0532
Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>
2020-11-19 10:11:37 +0100

    Simplify GridActions GUI
    Signed-off-by: Mohamed Hedi Khefacha <mohamed.hedi@rte-france.com>

--- src/utils/rest-api.js
+++ src/utils/rest-api.js
@@ -77,3 +77,26 @@ export function deleteListByName(name) {
         method: 'delete',
     });
 }
+
+/**
+ * Rename list by name
+ * @param oldNameList
+ * @param newNameList
+ * @returns {Promise<Response>}
+ */
+export function renameListByName(oldNameList, newNameList) {
+    const url =
+        PREFIX_ACTIONS_QUERIES +
+        '/v1/contingency-lists/' +
+        encodeURIComponent(oldNameList) +
+        '/rename';
+
+    return backendFetch(url, {
+        method: 'post',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ newContingencyListName: newNameList }),
+    });
+}
