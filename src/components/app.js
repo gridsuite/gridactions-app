d1d7e2ac48965e3c4fb2e089646d6a015e73a382
etiennehomer <etiennehomer@gmail.com>
2021-04-28 15:50:29 +0200

    Change language (#41)
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/components/app.js
+++ src/components/app.js
@@ -19,7 +19,12 @@ import {
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { LIGHT_THEME, selectTheme } from '../redux/actions';
+import {
+    LIGHT_THEME,
+    selectTheme,
+    selectLanguage,
+    selectComputedLanguage,
+} from '../redux/actions';
 
 import {
     TopBar,
@@ -50,7 +55,9 @@ import {
     APP_NAME,
     COMMON_APP_NAME,
     PARAMS_THEME_KEY,
+    PARAMS_LANGUAGE_KEY,
 } from '../utils/config-params';
+import { getComputedLanguage } from '../utils/language';
 
 const lightTheme = createMuiTheme({
     palette: {
@@ -78,6 +85,8 @@ const noUserManager = { instance: null, error: null };
 
 const App = () => {
     const theme = useSelector((state) => state.theme);
+
+    const language = useSelector((state) => state.language);
 
     const user = useSelector((state) => state.user);
 
@@ -164,6 +173,14 @@ const App = () => {
                     case PARAMS_THEME_KEY:
                         dispatch(selectTheme(param.value));
                         break;
+                    case PARAMS_LANGUAGE_KEY:
+                        dispatch(selectLanguage(param.value));
+                        dispatch(
+                            selectComputedLanguage(
+                                getComputedLanguage(param.value)
+                            )
+                        );
+                        break;
                     default:
                 }
             });
@@ -217,6 +234,10 @@ const App = () => {
         updateConfigParameter(PARAMS_THEME_KEY, theme);
     };
 
+    const handleLanguageClick = (language) => {
+        updateConfigParameter(PARAMS_LANGUAGE_KEY, language);
+    };
+
     return (
         <ThemeProvider theme={getMuiTheme(theme)}>
             <SnackbarProvider hideIconVariant={false}>
@@ -240,6 +261,8 @@ const App = () => {
                         appsAndUrls={appsAndUrls}
                         onThemeClick={handleThemeClick}
                         theme={theme}
+                        onLanguageClick={handleLanguageClick}
+                        language={language}
                         onAboutClick={() => console.debug('about')}
                     />
                     <Parameters
